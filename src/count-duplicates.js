e41248b5264b03cdef7ffc8a750c6342c2839f5a
Taimoor Aslam <taymooraslam@gmail.com>
2017-10-18 21:33:32 +0500

    Fix indendation

--- src/count-duplicates.js
+++ src/count-duplicates.js
@@ -5,24 +5,24 @@ export default function (str) {
   //validate string is alphanumeric
   const regex = /^[a-z0-9]+$/;
   if(!RegExp(regex, 'i').test(str))
-    throw new Error('String must be aplhanumeric');
+  throw new Error('String must be aplhanumeric');
   
   let count = 0;
   let frequencies = {};
   
   //calculate frequency for each character
   for(let char of str) {
-  	char = char.toLowerCase();
+    char = char.toLowerCase();
     if(frequencies[char])
-      frequencies[char]++;
+    frequencies[char]++;
     else
-      frequencies[char] = 1;
+    frequencies[char] = 1;
   }
-
+  
 	//count characters that are appeared more than once
   for(let char of Object.keys(frequencies)) {
     if(frequencies[char] > 1)
-      count++;
+    count++;
   }
   
   return count;
--- src/count-duplicates.spec.js
+++ src/count-duplicates.spec.js
@@ -6,24 +6,24 @@ describe('countDuplicates()', function() {
     const count = countDuplicates('aabb44');
     should.equal(count, 3);
   });
-
+  
   it('should throw an error if string is not alphanumeric', function(){
     try {
-    	countDuplicates('aabb44$%');
+      countDuplicates('aabb44$%');
     }
     catch(actual) {
 			let expected = new Error('String must be aplhanumeric');    
-    	should.deepEqual(actual, expected);
+      should.deepEqual(actual, expected);
     }
   });
-
+  
 	it('should return 0 if string is not provided', function() {
-  	const count = countDuplicates();
-  	should.equal(count, 0);
+    const count = countDuplicates();
+    should.equal(count, 0);
   });
   
   it('should return 0 if string is empty', function() {
-  	const count = countDuplicates();
-  	should.equal(count, 0);
+    const count = countDuplicates();
+    should.equal(count, 0);
   });
 });
